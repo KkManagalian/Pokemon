ff69cf1d33f68cba661d3b9e30b274ec186510df
KkManagalian <Dumbassyahoo2@gmail.com>
2025-11-07 09:18:35 +0200

    Pievienotas sakum klases

---
+++ src/pokemons/Pokedatnis.java
@@ -0,0 +1,13 @@
+package pokemons;
+
+public class Pokedatnis {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+
+	}
+	
+}
---
+++ src/pokemons/Pokemons.java
@@ -0,0 +1,7 @@
+package pokemons;
+
+public class Pokemons {
+
+	
+
+}
