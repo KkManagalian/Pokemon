00d723458d0218c03074802dc1ae4f1ebc9b41e4
KkManagalian <Dumbassyahoo2@gmail.com>
2025-11-14 09:35:08 +0200

    Izveidoti atributi

--- src/pokemons/ElektriskaisP.java
+++ src/pokemons/ElektriskaisP.java
@@ -1,5 +1,9 @@
 package pokemons;
 
-public class ElektriskaisP {
+public class ElektriskaisP extends Pokemons {
 
+	ElektriskaisP(String Vards, String Trenneris, String Uzbrukums, int Dziviba, int Limenis, int UzbrukumDz,
+			int Aizsardziba) {
+		super(Vards, Trenneris, Uzbrukums, Dziviba, Limenis, UzbrukumDz, Aizsardziba);
+}
 }
--- src/pokemons/Pokemons.java
+++ src/pokemons/Pokemons.java
@@ -2,6 +2,102 @@ package pokemons;
 
 public class Pokemons {
 
+	private String vards;
+	private String trenneris;
+	private String uzbrukums;
+	private int dziviba;
+	private int limenis;
+	private int uzbrukumDz;
+	private int aizsardziba;
 	
+	Pokemons(String Vards, String Trenneris, String Uzbrukums, int Dziviba, int Limenis, int UzbrukumDz, int Aizsardziba){
+		this.setVards(Vards);
+		this.setTrenneris(Trenneris);
+		this.setUzbrukums(Uzbrukums);
+		this.setDziviba(Dziviba);
+		this.setLimenis(Limenis);
+		this.setUzbrukumDz(UzbrukumDz);
+		this.setAizsardziba(Aizsardziba);
+	}
+	
+	
+
+	public String getTrenneris() {
+		return trenneris;
+	}
+
+	public void setTrenneris(String trenneris) {
+		this.trenneris = trenneris;
+	}
+
+
+
+	public String getVards() {
+		return vards;
+	}
+
+	public void setVards(String vards) {
+		this.vards = vards;
+	}
+
+
+
+	public int getDziviba() {
+		return dziviba;
+	}
+
+
+
+	public void setDziviba(int dziviba) {
+		this.dziviba = dziviba;
+	}
+
+
+
+	public String getUzbrukums() {
+		return uzbrukums;
+	}
+
+
+
+	public void setUzbrukums(String uzbrukums) {
+		this.uzbrukums = uzbrukums;
+	}
+
+
+
+	public int getUzbrukumDz() {
+		return uzbrukumDz;
+	}
+
+
+
+	public void setUzbrukumDz(int uzbrukumDz) {
+		this.uzbrukumDz = uzbrukumDz;
+	}
+
+
+
+	public int getLimenis() {
+		return limenis;
+	}
+
+
+
+	public void setLimenis(int limenis) {
+		this.limenis = limenis;
+	}
+
+
+
+	public int getAizsardziba() {
+		return aizsardziba;
+	}
+
+
+
+	public void setAizsardziba(int aizsardziba) {
+		this.aizsardziba = aizsardziba;
+	}
 
 }
