cb6988922852595523662cf12dd5b55b8d3e9baf
KkManagalian <Dumbassyahoo2@gmail.com>
2025-11-17 13:15:38 +0200

    Pieliktas bildes un opcijas, nekas daudz

--- src/pokemons/Pokedatnis.java
+++ src/pokemons/Pokedatnis.java
@@ -3,46 +3,68 @@ package pokemons;
 import java.awt.Image;
 import java.io.File;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class Pokedatnis {
 	
+	public static Icon Bildes(String fails, int Izm1, int Izm2) {
+	ImageIcon bilde = new ImageIcon(new ImageIcon("Bildes"+File.separator+fails).getImage().
+				getScaledInstance(Izm1, Izm2, Image.SCALE_SMOOTH));
+	return bilde;
+	}
+	
 	public static void main(String[] args) {
 		
-		boolean pamet=false;
 		String izvele="";
 		String[] gulta= {"Pamodies","Turpini gulēt"};
 		
 		do {
-			izvele = (String)JOptionPane.showInputDialog(null, "Izvēlies darbību", "Izvēlne", JOptionPane.QUESTION_MESSAGE, null, gulta, gulta[0]);
+			izvele = (String)JOptionPane.showInputDialog(null, "Aija žužu", "Gulta", JOptionPane.QUESTION_MESSAGE, null, gulta, gulta[0]);
 			if(izvele == null) {
 				izvele="Turpini gulēt";
 			}
 			switch(izvele) {
 			case "Pamodies":
-				String[] Maja= {"Apskati sevi spoguli", "Apskaties datoru", "Runāt ar mammu", "Pamest māju"};
-				izvele = (String)JOptionPane.showInputDialog(null, "Izvēlies darbību", "Izvēlne", JOptionPane.QUESTION_MESSAGE, null, Maja, Maja[0]);
+				do {
+				String[] Maja= {"Apskati sevi spogulī", "Apskaties datoru", "Runāt ar mammu", "Pamest māju"};
+				izvele = (String)JOptionPane.showInputDialog(null, "Ko darīsi", "Istaba", JOptionPane.PLAIN_MESSAGE, Bildes("Istaba.jpg", 200, 170), Maja, Maja[0]);
+				boolean pamet=false;
+				String dzimte="";
+				
+				if(izvele==null) {
+					izvele="Akuu";
+				}
 				switch(izvele) {
+				
+				case"Apskati sevi spogulī":
+					String[] Dzimte= {"Puisis","Meitene"};
+					dzimte = (String)JOptionPane.showInputDialog(null, "Vai es biju puisis vai meitene?", "Spogulis", JOptionPane.PLAIN_MESSAGE, Bildes("Dzimte.jpg", 200, 200), Dzimte, Dzimte[0]);
+					break;
+				
 				case"Apskaties datoru":
-					
+					System.out.println(dzimte);
 					break;
 				case"Runāt ar mammu":
 					JOptionPane.showMessageDialog(null, "Aij bērniņ, tev šodien paliek 10 gadi!"
-							+ "Skrien nu pie profesora un dabūni savu pirmo pokemonu!","Mutere",JOptionPane.PLAIN_MESSAGE, new ImageIcon(new ImageIcon("Atteli"+File.separator+"Mamma.png").getImage().
-									getScaledInstance(120, 120, Image.SCALE_SMOOTH)));
+							+ "\nSkrien nu pie profesora un dabūni savu pirmo pokemonu!","Mutere",JOptionPane.PLAIN_MESSAGE, Bildes("Mamma.png", 150, 150));
 					break;
 				case"Pamest māju":
 	
 					break;
+					
+				case"Akuu":
+					JOptionPane.showMessageDialog(null, "Tu aizgāji atpakaļ gulēt");
 				}
-				break;
+				}while(izvele!="Akuu");
+				
 			case "Turpini gulēt":
-				JOptionPane.showMessageDialog(null, "Tu iemigi uz visu mūžu", "Labs sapnis", JOptionPane.INFORMATION_MESSAGE);
-				pamet=true;
+				JOptionPane.showMessageDialog(null, "Tu iemigi uz visu mūžu", "Labs sapnis", JOptionPane.PLAIN_MESSAGE, Bildes("Gulet.png",150,150));
+				System. exit(0);
 				break;
 			}
-		}while(pamet!=true);
+		}while(izvele!="Turpini gulēt");
 
 	}
 	
